796197178e151d77a9027b5c4364e52271b2ad7a
Саша Темчур <sashatemcur@MBP-Sasa.bbrouter>
2023-03-28 12:35:49 +0300

    All done

--- actions/database_unittest/models.py
+++ actions/database_unittest/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 
 class Clients(models.Model):
-    name = models.CharField('Name clients', max_length=250)
+    name = models.CharField('Name clients', max_length=100)
     description = models.CharField('Description', max_length=250)
     status = models.BooleanField('Status')
-    balance = models.FloatField('Balance', max_length=250)
-    reg_date = models.DateField('Register Date', max_length=250)
+    balance = models.FloatField('Balance', max_length=50)
+    reg_date = models.DateField('Register Date')
 
     
     def __str__(self):
@@ -18,15 +18,15 @@ class Clients(models.Model):
 
 
 class Cars(models.Model):
-    name = models.CharField('Name car', max_length=250)
-    description = models.CharField('Description', max_length=250)
+    name = models.CharField('Name car', max_length=75)
+    description = models.CharField('Description', max_length=300)
     price = models.FloatField('Price')
-    owners = models.IntegerField('Owners')
+    owners = models.CharField('Owners', max_length=120)
     max_speed = models.IntegerField('Speed')
 
     
     def __str__(self):
-        return self.name
+        return '%s, %s' % (self.name, self.owners)
 
     class Meta:
         verbose_name = "car"
@@ -34,11 +34,11 @@ class Cars(models.Model):
 
 
 class VoleybolTeams(models.Model):
-    name = models.CharField('Name', max_length=250)
+    name = models.CharField('Name', max_length=350)
     speed = models.IntegerField('Speed')
     price = models.FloatField('Price')
     jump = models.FloatField('Jump')
-    power = models.CharField('Power', max_length=250)
+    power = models.CharField('Power', max_length=25)
 
     
     def __str__(self):
--- actions/database_unittest/tests.py
+++ actions/database_unittest/tests.py
@@ -1,2 +1,76 @@
 from django.test import TestCase
+from .models import Clients, Cars, VoleybolTeams
 
+class YourTestClass(TestCase):
+
+    @classmethod
+    def setUp(self):
+        Clients.objects.create(name="Sasha", description="good programer kjsfhkdfhljkshdf", status=True, balance=4532.43, reg_date='2020-02-21')
+        Cars.objects.create(name='BMW', description='good cars very beatufil sklhdkjgsfjh', price=12311313.43, owners='Sasha', max_speed=250)
+        VoleybolTeams.objects.create(name='Denis', speed=15, price=1212.32, jump=265, power='34 km/h')
+
+
+    def test_clients(self):
+        clients = Clients.objects.get(id=1)
+        expected_object_name = '%s' % (clients.name)
+        self.assertEquals(expected_object_name, str(clients))
+
+
+    def test_cars(self):
+        cars = Cars.objects.get(id=1)
+        expected_object_name = '%s, %s' % (cars.name, cars.owners)
+        self.assertEquals(expected_object_name, str(cars))
+
+
+    def test_voleybol_teams(self):
+        team = VoleybolTeams.objects.get(id=1)
+        expected_object_name = '%s' % (team.name)
+        self.assertEquals(expected_object_name, str(team))
+
+
+    def test_clients_name(self):
+        clients = Clients.objects.get(id=1)
+        max_length = clients._meta.get_field('name').max_length
+        self.assertEquals(max_length, 100)
+
+
+    def test_clients_description(self):
+        clients = Clients.objects.get(id=1)
+        max_length = clients._meta.get_field('description').max_length
+        self.assertEquals(max_length, 250)
+
+
+    def test_clients_balance(self):
+        clients = Clients.objects.get(id=1)
+        max_length = clients._meta.get_field('balance').max_length
+        self.assertEquals(max_length, 50)
+
+
+    def test_cars_name(self):
+        cars = Cars.objects.get(id=1)
+        max_length = cars._meta.get_field('name').max_length
+        self.assertEquals(max_length, 75)
+
+
+    def test_cars_description(self):
+        cars = Cars.objects.get(id=1)
+        max_length = cars._meta.get_field('description').max_length
+        self.assertEquals(max_length, 300)
+
+
+    def test_cars_owners(self):
+        cars = Cars.objects.get(id=1)
+        max_length = cars._meta.get_field('owners').max_length
+        self.assertEquals(max_length, 120)
+
+
+    def test_team_name(self):
+        team = VoleybolTeams.objects.get(id=1)
+        max_length = team._meta.get_field('name').max_length
+        self.assertEquals(max_length, 350)
+
+
+    def test_team_description(self):
+        team = VoleybolTeams.objects.get(id=1)
+        max_length = team._meta.get_field('power').max_length
+        self.assertEquals(max_length, 25)
